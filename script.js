d53028501fe21e973d9fec53dbcc041736fa3266
AbhijitKr <abhijitkr600@gmail.com>
2024-01-02 22:39:14 +0530

    Adds the scroll up button

--- script.js
+++ script.js
@@ -13,3 +13,20 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 });
+
+function scrollToTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+}
+
+window.onscroll = function () {
+  var scrollButton = document.getElementById("scrollBtn");
+
+  if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
+    scrollButton.classList.add("show"); // Show the button
+  } else {
+    scrollButton.classList.remove("show"); // Hide the button
+  }
+};
